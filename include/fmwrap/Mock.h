0b8a61aad7fd6305e506954e76084a0f2ccab5a4
tibogens <tibogens@gmail.com>
2014-02-14 01:22:52 +0100

    added fmwrap (mock FMWrapper DLL)

---
+++ include/fmwrap/Mock.h
@@ -0,0 +1,32 @@
+/*
+libfmplug, a C++ library to create FileMaker Pro (TM) plugins.
+Copyright (C) 2014 University of Lausanne, Switzerland
+Author: Thibault Genessay
+https://github.com/tibogens/libfmplug
+
+This file is part of libfmplug.
+
+libfmplug is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+libfmplug is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with libfmplug.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
+#ifndef _FMWRAP_MOCK_H
+#define _FMWRAP_MOCK_H
+
+namespace fmx
+{
+	__declspec(dllexport) void foobar();
+};
+
+#endif //_FMWRAP_MOCK_H
---
+++ src/fmwrap/pch.h
@@ -0,0 +1,38 @@
+/*
+libfmplug, a C++ library to create FileMaker Pro (TM) plugins.
+Copyright (C) 2014 University of Lausanne, Switzerland
+Author: Thibault Genessay
+https://github.com/tibogens/libfmplug
+
+This file is part of libfmplug.
+
+libfmplug is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+libfmplug is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with libfmplug.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
+// Use precompiled headers only on Windows, otherwise it'll just slow down
+// other builds as they'd have to include all of the stdlib even when they
+// don't use it.
+#ifdef _WIN32
+
+#include <string>
+#include <map>
+#include <vector>
+#include <algorithm> 
+#include <functional> 
+#include <cctype>
+#include <locale>
+#include <sstream>
+
+#endif // _WIN32
